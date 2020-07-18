13627e4b24d40370e366c1b00beda4ec97af8af2
KiritoKunSama <luca.alfaro.rampinelli@gmail.com>
2020-07-18 14:28:17 -0300

    reset

--- Testando Zeep (SOAP)/TesteSoap.py
+++
@@ -1,4 +0,0 @@
-from zeep import Client
-client = Client('http://www.soapclient.com/xml/soapresponder.wsdl')
-result = client.service.Method1(bstrParam1 = 'KIRITO', bstrParam2= 'KUN')
-print(result)
